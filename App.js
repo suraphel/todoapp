2653bc4095ce5438e6e4ac1898a6f853b01816cd
Suraphel Mekasha <zewdusu@hotmail.com>
2021-05-21 11:54:35 +0200

    init commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+import Addtodos from './Addtodos';
+import Navbar from './Container/Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Container/Home';
+import About from './Container/About';
+import ContactUs from './Container/ContactUs';
+import Post from './Container/Post';
+
+class App extends Component {
+	state = {
+		todos: [
+			{ id: 1, content: 'buy some milk' },
+			{ id: 2, content: 'play some mario cart' },
+			{ id: 3, content: 'play some flight Simulator' }
+		]
+	};
+	deletetodo = (id) => {
+		const todos = this.state.todos.filter((todo) => {
+			return todo.id !== id;
+		});
+		this.setState({
+			todos: todos
+		});
+		console.log(id);
+	};
+
+	listUpdater = (todo) => {
+		todo.id = Math.random();
+		let todos = [ ...this.state.todos, todo ];
+		this.setState({
+			todos
+		});
+	};
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="todo-App container">
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/About" component={About} />
+						<Route path="/Contact" component={ContactUs} />
+						<Route path="/:post_id" component={Post} />
+					</Switch>
+
+					{/* <h1 className ="center blue-text">Todos</h1>       */}
+					{/* <Todos todos = {this.state.todos} deletetodo = {this.deletetodo} />
+          <Addtodos listUpdater = {this.listUpdater}/> */}
+				</div>
+			</BrowserRouter>
+		);
+	}
+}
+export default App;
---
+++ Container/Home.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+// import axios from "axios"; FOR API USE ONLY
+import { Link } from 'react-router-dom';
+import img from '../img.jpg';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+	//  THIS IS A SAMPLE FOR USING A RESTAPI !!!!!!!!!!!!!!!!!!!!!
+	// state = {
+	//     post: [ ]
+	// }
+	// componentDidMount(){
+	//     axios.get('https://jsonplaceholder.typicode.com/posts')
+	//         .then(res =>  {
+	//             console.log(res)
+	//             this.setState({
+	//             post: res.data.slice(0,10)
+	//         })
+	//     })
+	// }
+
+	render() {
+		// console.log(this.props)
+		const { post } = this.props;
+		console.log(post);
+		const postList = post.length ? (
+			post.map((post) => {
+				return (
+					<div className="post card" key={post.id}>
+						<img src={img} alt="Suri's trail pic" />
+						<div className="card-content">
+							<Link to={'/' + post.id}>
+								<span className="card-title red-text">
+									{post.title}
+								</span>
+							</Link>
+							<p> {post.body}</p>
+						</div>
+					</div>
+				);
+			})
+		) : (
+			<div> "There are no elements in the array yet" </div>
+		);
+		return (
+			<div className="container home">
+				<h4 className="center">Home</h4>
+				{postList}
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		post: state.post
+	};
+};
+
+// get the data from the Data store and map it to the component
+
+export default connect(mapStateToProps)(Home);
+// as connect is a function for a highOrder component
